ca3d69642468cb649c5a716daa67752860c0d343
Alessio Rapisarda <alessiorapisarda@tutanota.com>
2022-12-14 01:04:32 +0100

    Update Leap setup, add flint extension

--- .config/nvim/lua/plugins/leap.lua
+++ .config/nvim/lua/plugins/leap.lua
@@ -1,7 +1,12 @@
-local status_ok, leap = pcall(require, "leap")
-if not status_ok then
+local leap_status_ok, leap = pcall(require, "leap")
+if not leap_status_ok then
     return
 end
 
-leap.setup{}
-leap.set_default_keymaps()
+local flit_status_ok, flit = pcall(require, "flit")
+if not flit_status_ok then
+    return
+end
+
+leap.add_default_mappings()
+flit.setup()
